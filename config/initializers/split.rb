d67be9b1b8284d7b5b4319009376fbc9c5096dc0
Thiago Belem <contato@thiagobelem.net>
2014-08-01 18:35:49 -0300

    Fix Split config

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,5 +1,5 @@
 Split.configure do |config|
   config.algorithm = Split::Algorithms::Whiplash
-  config.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
 end
 
+Split.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
