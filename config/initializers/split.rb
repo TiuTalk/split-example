8e433099a60024db6b115cffce65fed71b7d39fc
Thiago Belem <contato@thiagobelem.net>
2014-08-01 18:28:12 -0300

    Use Whiplash algorithm

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,1 +1,5 @@
-Split.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
+Split.configure do |config|
+  config.algorithm = Split::Algorithms::Whiplash
+  config.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
+end
+
